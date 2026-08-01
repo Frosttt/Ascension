d7ed65b13d14b94aa76979dd10d38264d005dc01
Frosttz <frosttzdev@gmail.com>
2026-08-01 16:36:11 -0700

    velocity calc for move

--- scripts/combat/MovementComponent.gd
+++ scripts/combat/MovementComponent.gd
@@ -5,3 +5,11 @@ extends Node
 @export var acceleration: float = 1200.0
 @export var deceleration: float = 1600.0
 
+func CalculateVelocity(currentVelocity: Vector2, inputDirection: Vector2, delta: float) -> Vector2:
+    var targetVelocity: Vector2 = inputDirection * maxSpeed
+    var change: float = acceleration
+
+    if (inputDirection.is_zero_approx()):
+        change = deceleration
+    
+    return currentVelocity.move_toward(targetVelocity, change * delta)
