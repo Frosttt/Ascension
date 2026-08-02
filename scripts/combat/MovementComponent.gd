522f7a3ed0605aca4c6751cb1fd5e2a0dc83f76e
Frosttz <frosttzdev@gmail.com>
2026-08-01 22:35:21 -0700

    Cardinal facing direction and camera smoothing

--- scripts/combat/MovementComponent.gd
+++ scripts/combat/MovementComponent.gd
@@ -6,10 +6,10 @@ extends Node
 @export var deceleration: float = 1600.0
 
 func CalculateVelocity(currentVelocity: Vector2, inputDirection: Vector2, delta: float) -> Vector2:
-    var targetVelocity: Vector2 = inputDirection * maxSpeed
-    var change: float = acceleration
+	var targetVelocity: Vector2 = inputDirection * maxSpeed
+	var change: float = acceleration
 
-    if (inputDirection.is_zero_approx()):
-        change = deceleration
-    
-    return currentVelocity.move_toward(targetVelocity, change * delta)
+	if (inputDirection.is_zero_approx()):
+		change = deceleration
+	
+	return currentVelocity.move_toward(targetVelocity, change * delta)
--- scripts/player/PlayerCharacter.gd
+++ scripts/player/PlayerCharacter.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 
 @onready var moveComponent: MovementComponent = $MovementComponent
 
+enum Direction { RIGHT, UP, LEFT, DOWN }
+
+var faceDirection: Direction = Direction.DOWN
+
 func _ready() -> void:
 	pass
 
@@ -16,10 +20,29 @@ func _physics_process(delta: float) -> void:
 	# Probably move this to it's own component later
 
 	var inputDirection: Vector2 = Input.get_vector(
-		"MoveLeft", 
-		"MoveRight", 
-		"MoveUp", 
-		"MoveDown")
+		"Move_Left", 
+		"Move_Right", 
+		"Move_Up", 
+		"Move_Down")
+	
+	if (not inputDirection.is_zero_approx()):
+		faceDirection = GetCardinalDirection(inputDirection)
+	
 	velocity = moveComponent.CalculateVelocity(velocity, inputDirection, delta)
 
 	move_and_slide()
+	
+	
+func GetCardinalDirection(input_direction: Vector2) -> Direction:
+	var angle := fposmod(input_direction.angle(), TAU)
+
+	if angle < PI / 4.0 or angle >= 7.0 * PI / 4.0:
+		return Direction.RIGHT
+
+	if angle < 3.0 * PI / 4.0:
+		return Direction.DOWN
+
+	if angle < 5.0 * PI / 4.0:
+		return Direction.LEFT
+
+	return Direction.UP
