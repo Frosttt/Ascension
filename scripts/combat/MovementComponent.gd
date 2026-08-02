4e4eac3f9e8e238811022898c08b4dff6e43ac58
Frosttz <frosttzdev@gmail.com>
2026-08-01 22:53:02 -0700

    Added temp sprites and animation

--- scripts/combat/MovementComponent.gd
+++ scripts/combat/MovementComponent.gd
@@ -5,6 +5,8 @@ extends Node
 @export var acceleration: float = 1200.0
 @export var deceleration: float = 1600.0
 
+enum Direction { RIGHT, UP, LEFT, DOWN }
+
 func CalculateVelocity(currentVelocity: Vector2, inputDirection: Vector2, delta: float) -> Vector2:
 	var targetVelocity: Vector2 = inputDirection * maxSpeed
 	var change: float = acceleration
@@ -13,3 +15,18 @@ func CalculateVelocity(currentVelocity: Vector2, inputDirection: Vector2, delta:
 		change = deceleration
 	
 	return currentVelocity.move_toward(targetVelocity, change * delta)
+
+
+static func GetCardinalDirection(input_direction: Vector2) -> Direction:
+	var angle := fposmod(input_direction.angle(), TAU)
+
+	if angle < PI / 4.0 or angle >= 7.0 * PI / 4.0:
+		return Direction.RIGHT
+
+	if angle < 3.0 * PI / 4.0:
+		return Direction.DOWN
+
+	if angle < 5.0 * PI / 4.0:
+		return Direction.LEFT
+
+	return Direction.UP
--- scripts/player/PlayerCharacter.gd
+++ scripts/player/PlayerCharacter.gd
@@ -2,10 +2,9 @@ class_name PlayerCharacter
 extends CharacterBody2D
 
 @onready var moveComponent: MovementComponent = $MovementComponent
+@onready var sprite: Sprite2D = $Sprite
 
-enum Direction { RIGHT, UP, LEFT, DOWN }
-
-var faceDirection: Direction = Direction.DOWN
+var faceDirection: MovementComponent.Direction = MovementComponent.Direction.DOWN
 
 func _ready() -> void:
 	pass
@@ -26,23 +25,15 @@ func _physics_process(delta: float) -> void:
 		"Move_Down")
 	
 	if (not inputDirection.is_zero_approx()):
-		faceDirection = GetCardinalDirection(inputDirection)
+		faceDirection = MovementComponent.GetCardinalDirection(inputDirection)
 	
+	if (faceDirection == MovementComponent.Direction.LEFT):
+		sprite.scale.x = -abs(sprite.scale.x)
+	elif (faceDirection == MovementComponent.Direction.RIGHT):
+		sprite.scale.x = abs(sprite.scale.x)
+
 	velocity = moveComponent.CalculateVelocity(velocity, inputDirection, delta)
 
 	move_and_slide()
 	
 	
-func GetCardinalDirection(input_direction: Vector2) -> Direction:
-	var angle := fposmod(input_direction.angle(), TAU)
-
-	if angle < PI / 4.0 or angle >= 7.0 * PI / 4.0:
-		return Direction.RIGHT
-
-	if angle < 3.0 * PI / 4.0:
-		return Direction.DOWN
-
-	if angle < 5.0 * PI / 4.0:
-		return Direction.LEFT
-
-	return Direction.UP
