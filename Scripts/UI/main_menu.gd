cec795749af4f00843e448c3dfbc72828fdd048a
Frosttz <frosttzdev@gmail.com>
2026-09-07 16:33:54 -0700

    Added tween animation to main menu

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -1,6 +1,44 @@
 extends Control
 class_name MainMenu
 
+@onready var _title: Label = $GlobalMenuContainer/MenuGroup/MainMenuVContainer/TitleText
+@onready var _button_container: VBoxContainer= $GlobalMenuContainer/MenuGroup/MainMenuVContainer/MarginContainer/MenuButtonContainer
+var _intro_tween: Tween;
+@export var animation_duration: float= .75;
+@export var stagger_delay: float = .05
+
+func _ready() -> void:
+	play_intro_animation()
+
+
+func play_intro_animation() -> void:
+	_title.offset_transform_position.x = -720;
+	var buttonlist: Array[Node] = _button_container.get_children()
+
+	if (_intro_tween != null):
+		_intro_tween.kill();
+
+	_intro_tween = create_tween()
+	_intro_tween.set_parallel(true)
+	
+	_intro_tween.tween_property(
+		_title,
+		"offset_transform_position:x",
+		0.0,
+		animation_duration
+	).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+
+	for index: int in buttonlist.size():
+		var control : Control = buttonlist.get(index) as Control
+		if (!control.visible || !control.offset_transform_enabled):
+			continue
+		control.offset_transform_position.x = -720;
+		_intro_tween.tween_property(
+			control,
+			"offset_transform_position:x",
+			0.0,
+			animation_duration
+		).set_delay((index + 1) * stagger_delay).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 
 
 func _on_quit_button_button_up() -> void:
